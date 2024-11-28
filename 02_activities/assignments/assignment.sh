4b1790cf5452ab913f3b67f5837beed29a595a63
Dion <dionysius.indraatmadja@gmail.com>
2024-11-27 19:03:03 -0500

    fixed paths

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -32,12 +32,12 @@ ls data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 mkdir -p ./data/processed/server_logs ./data/processed/user_logs ./data/processed/event_logs  # create subfolders and parents
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cp ./data/raw/rawdata/*server*.log ./data/processed/server_logs  # copy server log files to server_log folder
+cp ./data/raw/*server*.log ./data/processed/server_logs  # copy server log files to server_log folder
 # 6. Repeat the above step for user logs and event logs
-cp ./data/raw/rawdata/*user*.log ./data/processed/user_logs  # copy user log files to user_logs folder
-cp ./data/raw/rawdata/*event*.log ./data/processed/event_logs  # copy event log files to event_logs folder
+cp ./data/raw/*user*.log ./data/processed/user_logs  # copy user log files to user_logs folder
+cp ./data/raw/*event*.log ./data/processed/event_logs  # copy event log files to event_logs folder
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rm ./data/processed/user_logs/*ipaddr* ./data/raw/rawdata/*ipaddr*  # delte ipaddr files in both directories
+rm ./data/processed/user_logs/*ipaddr* ./data/raw/*ipaddr*  # delte ipaddr files in both directories
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 ls -R ./data/processed > ./data/inventory.txt  # -R recursively list all subfolders and files under ./data/processed. 
 
